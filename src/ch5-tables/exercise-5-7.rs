a3db94249407751006e3b4151f8d170193860242
Shane Bishop <shane.bishop@outlook.com>
2023-07-30 19:18:20 -0400

    Fixed exercise 5.7

--- src/ch5-tables/exercise-5-7.rs
+++ src/ch5-tables/exercise-5-7.rs
@@ -4,13 +4,18 @@ position of another given list.
 */
 
 fn insert_vec(insert_into: &mut Vec<i32>, index: usize, to_insert: &Vec<i32>) {
-    let old_len = to_insert.len();
+    let old_len = insert_into.len();
+
+    if index > old_len {
+        panic!("index out of bounds");
+    }
+
     let to_insert_len = to_insert.len();
 
     // Reserve space for new elements.
     // Note that Vec::reserve doesn't increase the length, so assignments after
     // Vec::reserve would not work, which is why we use Vec::resize instead.
-    insert_into.resize(to_insert_len, 0);
+    insert_into.resize(old_len + to_insert_len, 0);
 
     // Shift elements in insert_into down
     for i in index..old_len {
@@ -26,5 +31,17 @@ fn insert_vec(insert_into: &mut Vec<i32>, index: usize, to_insert: &Vec<i32>) {
 fn main() {
     let mut v = vec![1,2,3];
     insert_vec(&mut v, 1, &vec![4,5]);
-    assert_eq!(v, vec![1,4,5,2,3])
+    assert_eq!(v, vec![1,4,5,2,3]);
+
+    let mut v = vec![];
+    insert_vec(&mut v, 0, &vec![]);
+    assert_eq!(v, vec![]);
+
+    let mut v = vec![];
+    insert_vec(&mut v, 0, &vec![1,2,3]);
+    assert_eq!(v, vec![1,2,3]);
+
+    let mut v = vec![1,2,3];
+    insert_vec(&mut v, 0, &vec![]);
+    assert_eq!(v, vec![1,2,3]);
 }
