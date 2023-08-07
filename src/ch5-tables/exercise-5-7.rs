2087a37d0a82270e78a68defd2b1b945c1dcaede
Shane Bishop <shane.bishop@outlook.com>
2023-08-07 11:16:10 -0400

    Made insert_vec for exercise 5.7 generic

--- src/ch5-tables/exercise-5-7.rs
+++ src/ch5-tables/exercise-5-7.rs
@@ -3,7 +3,11 @@ Write a function that inserts all elements of a given list into a given
 position of another given list.
 */
 
-fn insert_vec(insert_into: &mut Vec<i32>, index: usize, to_insert: &Vec<i32>) {
+use std::default::Default;
+
+fn insert_vec<T>(insert_into: &mut Vec<T>, index: usize, to_insert: &Vec<T>)
+    where T: Default + Copy
+{
     let old_len = insert_into.len();
 
     if index > old_len {
@@ -14,8 +18,10 @@ fn insert_vec(insert_into: &mut Vec<i32>, index: usize, to_insert: &Vec<i32>) {
 
     // Reserve space for new elements.
     // Note that Vec::reserve doesn't increase the length, so assignments after
-    // Vec::reserve would not work, which is why we use Vec::resize instead.
-    insert_into.resize(old_len + to_insert_len, 0);
+    // Vec::reserve would not work. Vec::resize also does not work if we want to
+    // use generic code, since Vec::resize requires a value to fill new elements
+    // with, which is why we use Vec::resize_with instead.
+    insert_into.resize_with(old_len + to_insert_len, Default::default);
 
     // Shift elements in insert_into down
     for i in index..old_len {
@@ -33,11 +39,11 @@ fn main() {
     insert_vec(&mut v, 1, &vec![4,5]);
     assert_eq!(v, vec![1,4,5,2,3]);
 
-    let mut v = vec![];
+    let mut v: Vec<i32> = vec![];
     insert_vec(&mut v, 0, &vec![]);
     assert_eq!(v, vec![]);
 
-    let mut v = vec![];
+    let mut v: Vec<i32> = vec![];
     insert_vec(&mut v, 0, &vec![1,2,3]);
     assert_eq!(v, vec![1,2,3]);
 
