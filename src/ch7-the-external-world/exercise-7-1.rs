e6991d74635ad9ece76bdd764c9c3bc8ea36eb07
Shane Bishop <shane.bishop@outlook.com>
2023-08-07 13:10:07 -0400

    Exercise 7.1 is working now

--- src/ch7-the-external-world/exercise-7-1.rs
+++ src/ch7-the-external-world/exercise-7-1.rs
@@ -10,33 +10,44 @@ NOTE: This does not handle read or write failures.
 */
 
 use std::env;
-// use std::io::stdin;
-// use std::os::fd::OwnedFd;
 use std::process::exit;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
 
-fn main() -> std::io::Result<()> {
+fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 1 {
+    if args.len() < 2 {
         eprintln!("Error: Missing input file argument.");
         exit(1);
     }
 
-    let f = File::open(&args[0])?;
+    let path = &args[1];
+
+    let f = match File::open(path) {
+        Err(why) => die(format!("Failed to open input file '{}': {}", path, why)),
+        Ok(file) => file,
+    };
 
     let mut lines = Vec::<String>::new();
     for res in BufReader::new(f).lines() {
-        lines.push(res.expect("Failed to read all lines from input file"));
+        match res {
+            Err(why) => die(format!("Failed to read all lines from '{}': {}", path, why)),
+            Ok(line) => lines.push(line),
+        };
     }
 
     lines.sort();
 
     for line in lines {
-        println!(line);
+        println!("{}", line);
     }
 
     exit(0);
+}
+
+fn die(s: String) -> ! {
+    eprintln!("{}", s);
+    exit(1);
 }
