cef2fe333694cd8e8df7bca778178fa7c809e6d3
Shane Bishop <shane.bishop@outlook.com>
2023-08-20 10:25:11 -0400

    Exercise 7.1 now can read from either stdin or from a file

--- README.md
+++ README.md
@@ -9,6 +9,6 @@ Ierusalimschy](https://www.lua.org/pil/#4ed), but for Rust.
 As each exercise is a standalone program, no `Cargo.toml` is used. Instead,
 each exercise can be compiled and run separately. For example:
 ```bash
-rustc src/ch1-getting-started/exercise-1-8.rs
+rustc --edition 2021 src/ch1-getting-started/exercise-1-8.rs
 ./exercise-1-8 # Or .\exercise-1-8.exe on Windows
 ```
--- src/ch7-the-external-world/exercise-7-1.rs
+++ src/ch7-the-external-world/exercise-7-1.rs
@@ -1,37 +1,41 @@
 /*
-Write a program that reads a text file and rewrites it with its lines sorted
+Write a program that reads a text file and outputs it with its lines sorted
 in alphabetical order. When called with no arguments, it should read from
 standard input and write to standard output. When called with one file-name
 argument, it should read from that file and write to standard output. When
 called with two file-name arguments, it should read from the first file and
 write to the second.
-
-NOTE: This does not handle read or write failures.
 */
 
 use std::env;
 use std::process::exit;
 use std::fs::File;
+use std::io::Read;
 use std::io::BufReader;
 use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 2 {
-        eprintln!("Error: Missing input file argument.");
-        exit(1);
-    }
+    let (reader, path) =
+        if args.len() > 1 {
+            let path = &args[1];
 
-    let path = &args[1];
+            let f = match File::open(path) {
+                Err(why) => die(format!("Failed to open input file '{}': {}", path, why)),
+                Ok(file) => file,
+            };
 
-    let f = match File::open(path) {
-        Err(why) => die(format!("Failed to open input file '{}': {}", path, why)),
-        Ok(file) => file,
-    };
+            let reader: Box<dyn Read> = Box::new(f);
+
+            (reader, path.clone())
+        } else {
+            let reader: Box<dyn Read> = Box::new(std::io::stdin());
+            (reader, String::from("<stdin>"))
+        };
 
     let mut lines = Vec::<String>::new();
-    for res in BufReader::new(f).lines() {
+    for res in BufReader::new(reader).lines() {
         match res {
             Err(why) => die(format!("Failed to read all lines from '{}': {}", path, why)),
             Ok(line) => lines.push(line),
